e251fd4032dc30f4308721e2d1f5649354a5f396
Aung Khant Zin <aungkhantzin1364@gmail.com>
2026-04-21 23:31:53 +0800

    Resume Upload (new file)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,14 +47,14 @@ export default function Header() {
         </div>
         <div className="resume-actions">
           <a
-            href="/AUNGKHANTZIN_RESUME.pdf"
+            href="/RESUME_AUNGKHANTZIN.pdf"
             target="_blank"
             rel="noreferrer"
             className="btn-primary"
           >
             View resume
           </a>
-          <a href="/AUNGKHANTZIN_RESUME.pdf" download className="btn-secondary ghost">
+          <a href="/RESUME_AUNGKHANTZIN.pdf" download className="btn-secondary ghost">
             Download PDF
           </a>
         </div>
